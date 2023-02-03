d351cd3c417a3674b955736a1d101df15b016591
devourer3 <orvainqueur@gmail.com>
2023-02-03 23:51:32 +0900

    sliver 샘플

--- lib/ui/home/view/navigator.dart
+++ lib/ui/home/view/navigator.dart
@@ -8,11 +8,11 @@ class CustomNavigator extends StatefulWidget {
 
 
   @override
-  _CustomNavigatorState createState() => _CustomNavigatorState();
+  CustomNavigatorState createState() => CustomNavigatorState();
 }
 
 
-class _CustomNavigatorState extends State<CustomNavigator> with AutomaticKeepAliveClientMixin {
+class CustomNavigatorState extends State<CustomNavigator> with AutomaticKeepAliveClientMixin {
   @override
   bool get wantKeepAlive => true;
 
@@ -26,3 +26,15 @@ class _CustomNavigatorState extends State<CustomNavigator> with AutomaticKeepAli
     );
   }
 }
+
+// 덕분에 개발 잘할 수 있었습니다.
+// 제가 쓰다 보니까 맨 마지막 방법에서도 몇가지 문제점이 발견되었는데, 다른 분들도 참고 하시면 좋을 것 같아서 올려봅니다.
+//
+// 일단, pop을 할 때에 일반적인 방식으로 하게 되면 에러가 나서 navigatorKeyList에서 해당 NavigatorState를 들고 와서 pop을 해야 하고
+//
+// 두번째로는 tab 간에 전환시에 해당 tab이 위젯 트리에 올라와 있는 상태가 아니라 오류가 발생한다는 점입니다.
+// 이렇게만 말하면 어려울 것 같은데, 예를 들어, 4번째 tab에서 어떤 버튼을 누르면 2번째 tab으로 전환 후에 2번째 tab에서의 어떤 화면으로 전환하고자 한다고 하면, tabController의 index를 임의로 바꾸어준 후에 2번째 navigatorKeyList에서 currentContext를 가져와서 push를 해야 하는데 2번째 page에 한번도 근접했던 적이 없다면 currentContext가 null이라며 오류 메세지를 띄워주게 됩니다. 이는 아직 widget tree에 해당 화면의 customNavigator가 그려지지 않아서 발생하는 문제였고, 전 일단 50mill second마다 확인하는 식으로 해결했습니다.
+//
+// 세번째로는 scaffold가 앱 내에서 하나밖에 없어서 조금 불편합니다. flutter run --route로 특정화면에 접근하는 부분이 어렵고 다른 화면을 push할 때에 배경에 이전 화면의 잔상이 남아 있는 경우가 빈번히 발생해서 push하는 새로운 page에 배경색을 덧입혀 해결했습니다.
+//
+// 내용이 좀 길었는데 정말 덕분에 개발 잘 할 수 있었습니다. 감사합니다.
--- lib/ui/memory_list/view/memory_list.dart
+++ lib/ui/memory_list/view/memory_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class MemoryList extends StatefulWidget {
   @override
@@ -11,6 +12,32 @@ class MemoryList extends StatefulWidget {
 class _MemoryListState extends State<MemoryList> {
   @override
   Widget build(BuildContext context) {
-    return const Text('매모리리스트');
+    return CustomScrollView(
+      slivers: [
+        const SliverAppBar(
+          // Whether the app bar should remain visible at the start of the scroll view
+          // 스크롤을 내렸을 때 앱바가 남아 있을 것이냐
+          pinned: false,
+          // 사용자가 살짝 스크롤을 올렸을 때 appBar의 전체를 보여줄 것인가
+          floating: true,
+          // floating이랑 비슷합니다.. 솔직히 무슨 차이인지 모르겠습니다.
+          snap: false,
+          // 헤더의 최대 높이
+          expandedHeight: 150.0,
+          // 늘어나는 영역의 헤더 UI 정의
+          flexibleSpace: FlexibleSpaceBar(
+            title: Text('Sliver한 appbar입니다'),
+          ),
+        ),
+        SliverList(
+            delegate: SliverChildListDelegate([
+              Container(height: 200, color: Colors.red),
+              Container(height: 200, color: Colors.purple),
+              Container(height: 200, color: Colors.green),
+              Container(height: 200, color: Colors.orange),
+              Container(height: 200, color: Colors.yellow),
+            ]))
+      ]
+    );
   }
 }
