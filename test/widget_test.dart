842a7b309160bfce118c32d0128cea692177a15d
devourer3 <orvainqueur@gmail.com>
2023-02-07 22:21:58 +0900

    코드 수정

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:tome/main.dart';
+import 'package:tome/ui/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const Tome());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
