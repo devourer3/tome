024441267e2ca0ca2780fb0695f860cd95723d21
devourer3 <orvainqueur@gmail.com>
2023-02-08 21:42:28 +0900

    플로팅 버튼 추가

--- lib/ui/home/view/home.dart
+++ lib/ui/home/view/home.dart
@@ -1,8 +1,12 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:tome/ui/constants/colors.dart';
+import 'package:tome/ui/constants/routes.dart';
 import 'package:tome/ui/home/view/navigator.dart';
 import 'package:tome/ui/memory_list/view/memory_list.dart';
 import 'package:tome/ui/settings/view/settings.dart';
+import 'package:tome/ui/utils/theme_util.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -15,45 +19,37 @@ class _HomeState extends State<Home> {
   int _currentIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        return !(await _navigatorKeyList[_currentIndex].currentState!.maybePop());
-      },
-      child: DefaultTabController(
-        length: 2,
-        child: Scaffold(
-          body: TabBarView(
-            children: _pages.map(
-                  (page) {
+    //모바일 상단 상태 바 높이 값
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
+    return Container(
+      padding: EdgeInsets.only(top: statusBarHeight),
+      child: WillPopScope(
+        onWillPop: () async {
+          return !(await _navigatorKeyList[_currentIndex].currentState!.maybePop());
+        },
+        child: DefaultTabController(
+          length: _pages.length, // 탭 개수
+          child: Scaffold(
+            body: TabBarView(
+              children: _pages.map((page) {
                 int index = _pages.indexOf(page);
-                return CustomNavigator(
-                  page: page,
-                  navigatorKey: _navigatorKeyList[index],
-                );
+                return CustomNavigator(page: page, navigatorKey: _navigatorKeyList[index]);
               },
-            ).toList(),
-          ),
-          bottomNavigationBar: TabBar(
-            isScrollable: false,
-            indicatorPadding: const EdgeInsets.only(bottom: 74),
-            automaticIndicatorColorAdjustment: true,
-            onTap: (index) => setState(() {
-              _currentIndex = index;
-            }),
-            tabs: const [
-              Tab(
-                icon: Icon(
-                  Icons.list_alt_sharp,
-                ),
-                text: '메모리',
-              ),
-              Tab(
-                icon: Icon(
-                  Icons.settings,
-                ),
-                text: '세팅',
-              ),
-            ],
+              ).toList(),
+            ),
+            bottomNavigationBar: TabBar(
+              isScrollable: false,
+              indicatorPadding: const EdgeInsets.only(bottom: 74),
+              automaticIndicatorColorAdjustment: true,
+              onTap: (index) =>
+                  setState(() {
+                    _currentIndex = index;
+                  }),
+              tabs: const [
+                Tab(icon: Icon(Icons.list_alt_sharp), text: '메모리'),
+                Tab(icon: Icon(Icons.settings), text: '세팅'),
+              ],
+            ),
           ),
         ),
       ),
--- lib/ui/memory_list/view/memory_list.dart
+++ lib/ui/memory_list/view/memory_list.dart
@@ -1,10 +1,14 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:tome/data/db/model/memory.dart';
 import 'package:tome/ui/Intro/bloc/intro_memo_bloc.dart';
 import 'package:tome/ui/Intro/bloc/intro_memo_state.dart';
+import 'package:tome/ui/constants/colors.dart';
+import 'package:tome/ui/constants/routes.dart';
 import 'package:tome/ui/memory_list/view/memory_list_item.dart';
+import 'package:tome/ui/utils/theme_util.dart';
 
 class MemoryList extends StatefulWidget {
   @override
@@ -19,24 +23,32 @@ class _MemoryListState extends State<MemoryList> {
     return BlocConsumer<MemoryBloc, MemoryState>(
         builder: (context, state) {
           List<MemoryItemModel> list = state.memoryList;
-          return CustomScrollView(slivers: [
-            // const SliverAppBar(
-            // Whether the app bar should remain visible at the start of the scroll view
-            // 스크롤을 내렸을 때 앱바가 남아 있을 것이냐
-            // pinned: false, // 사용자가 살짝 스크롤을 올렸을 때 appBar 의 전체를 보여줄 것인가
-            // snap: false, // 헤더의 최대 높이
-            // floating: true,
-            // expandedHeight: 150.0, // 늘어나는 영역의 헤더 UI 정의
-            // flexibleSpace: FlexibleSpaceBar(
-            //   title: Text('Sliver한 appbar입니다'),
-            // ),
-            // ),
-            SliverList(
-                delegate: SliverChildBuilderDelegate(childCount: list.length, (context, index) {
-                  MemoryItemModel listItem = list[index];
-              return MemoryListItem(question: listItem.question, answer: listItem.answer);
-            }))
-          ]);
+          return Scaffold(
+            floatingActionButton: FloatingActionButton(
+              autofocus: true,
+              shape: StadiumBorder(side: BorderSide(color: ThemeColor(context: context, name: ColorName.white), width: 1)),
+              child: Text(tr('generate')),
+              onPressed: () => Navigator.restorablePushNamed(context, Routes.urlIntro),
+            ),
+            body: CustomScrollView(slivers: [
+              // const SliverAppBar(
+              // Whether the app bar should remain visible at the start of the scroll view
+              // 스크롤을 내렸을 때 앱바가 남아 있을 것이냐
+              // pinned: false, // 사용자가 살짝 스크롤을 올렸을 때 appBar 의 전체를 보여줄 것인가
+              // snap: false, // 헤더의 최대 높이
+              // floating: true,
+              // expandedHeight: 150.0, // 늘어나는 영역의 헤더 UI 정의
+              // flexibleSpace: FlexibleSpaceBar(
+              //   title: Text('Sliver한 appbar입니다'),
+              // ),
+              // ),
+              SliverList(
+                  delegate: SliverChildBuilderDelegate(childCount: list.length, (context, index) {
+                    MemoryItemModel listItem = list[index];
+                    return MemoryListItem(question: listItem.question, answer: listItem.answer);
+                  }))
+            ]),
+          );
         },
         listener: (context, state) {});
   }
